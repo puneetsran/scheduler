fd82646d95dbc8663d5fcf67eaa8981b398ab977
Puneet Sran <puneet_sran@icloud.com>
2019-10-29 17:14:46 -0700

    combine all of the state in Application component into a single object called state, update props, add setDay and setDays functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -48,14 +48,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  const setDays = days => setState(prev => ({ ...prev, days }));
   useEffect(() => {
-    axios.get(`/api/days`).then((response) => {
-      console.log(response.data);
-      setDays(response.data);
-    });
+    axios.get('/api/days').then(response => setDays(response.data));
   }, []);
 
   return (
@@ -69,8 +73,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
