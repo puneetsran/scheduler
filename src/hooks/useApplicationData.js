ba1c1094f72be74e4a40a28320efa7e9647ba7e1
Puneet Sran <puneet_sran@icloud.com>
2019-11-04 20:24:08 -0800

    move reducer function into its own file, create reducer test file with one test to increate test coverage

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,42 +1,10 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "SET_DAY":
-      return { ...state, day: action.day };
-
-    case "SET_APPLICATION_DATA":
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-      };
-
-    case "SET_INTERVIEW": {
-      const appointment = {
-        ...state.appointments[action.id],
-        interview: action.interview || null
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment
-      };
-      return { ...state, appointments };
-    }
-
-    case "SET_SPOTS": {
-      return { ...state, days: action.value };
-    }
-
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
