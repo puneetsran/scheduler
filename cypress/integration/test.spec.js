eda09817978df01cb7fea89cc146c3f25280aafe
Puneet Sran <puneet_sran@icloud.com>
2019-11-05 15:13:10 -0800

    write test for navigating days

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -5,8 +5,22 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get("li")
-      .contains("Tuesday")
-      .click();
+    // cy.get("li")
+    //   .contains("Tuesday")
+    //   .click();
+
+    // cy.contains("li", "Tuesday").should(
+    //   "have.css",
+    //   "background-color",
+    //   "rgb(242, 242, 242)"
+    // );
+
+    // cy.contains("li", "Tuesday")
+    //   .click()
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
